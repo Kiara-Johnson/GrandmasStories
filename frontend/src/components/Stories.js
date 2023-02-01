00530c60103f471e56883436ea89f38969cc178e
Kiara Johnson <johns.kia35@gmail.com>
2023-02-01 09:01:50 -0700

    Tailwind working

--- frontend/src/components/Stories.js
+++ frontend/src/components/Stories.js
@@ -1,19 +1,18 @@
 import React from "react";
+
 import Story from "./story/Story";
 
 export default function Stories(story) {
-    // const stories = useSelector((state) => state.stories);
+    // const stories = useSelector((state) => state.story);
   return (
-    <div>
-        <Story />
-        <Story />
-    </div>
-    // stories.length (
-    //       {story.map((story) => (
-    //         {story._id}
-    //           <Story story={story} />
-    //       ))}
-    //   )
-
-  );
+    !stories.length ? <CircularProgress /> : (
+      <Grid className='grid-cols-3'>
+        {stories.map((story) => (
+          <Grid key={story._id} item xs={12} sm={6}>
+            <Story story={story} />
+          </Grid>
+        ))}
+      </Grid>
+      )
+  )
 }
